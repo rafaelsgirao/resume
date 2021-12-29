46af2575e150dbfa348e03ab47d9355c66095dd0
Rafael Girão <git@rafael.ovh>
2021-12-29 16:19:01 +0000

    Update html and pdf

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-
+#nix-shell -p pandoc wkhtmltopdf --run ./run.sh
 set -e
+touch resume.html resume.pdf
 rm resume.html resume.pdf
 
 pandoc --self-contained -o resume.html -c stylesheet.css resume.md
